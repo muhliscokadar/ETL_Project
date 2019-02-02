2d5873fcae878653e007cf5ad4f476d271303817
bharathiyer <bharathiyer@gmail.com>
2019-02-02 14:43:48 -0600

    ETL notebook and updated schema/query files

--- etl/query.sql
+++ etl/query.sql
@@ -1,5 +1,21 @@
 USE outbreak_db;
 
 -- Query to check successful load
+SELECT * FROM food_data;
 
--- Join tables on county_id
+SELECT * FROM temperature;
+
+-- Join tables on yearmonth and state. Sum up the number of illnesses, hospitalizations and deaths.
+SELECT 
+    food_data.yearmonth,
+    food_data.state,
+    SUM(IFNULL(food_data.illnesses, 0)) AS total_illnesses,
+    SUM(IFNULL(food_data.hospitalizations, 0)) AS total_hospitalizations,
+    SUM(IFNULL(food_data.deaths, 0)) AS total_deaths,
+    temperature.average_temp
+FROM
+    food_data
+        INNER JOIN
+    temperature ON food_data.yearmonth = temperature.yearmonth
+        AND food_data.state = temperature.state
+GROUP BY food_data.yearmonth , food_data.state;
--- etl/schema.sql
+++ etl/schema.sql
@@ -5,25 +5,25 @@ CREATE DATABASE outbreak_db;
 USE outbreak_db;
 
 -- Create Two Tables
-CREATE TABLE food_data(
-	id INT(10) PRIMARY KEY NOT NULL,
+CREATE TABLE food_data (
+    id INT(10) PRIMARY KEY NOT NULL,
     yearmonth DATE,
     state VARCHAR(50),
-    genus_species VARCHAR (50), 
-    etiology_status VARCHAR (20),
-    location_of_preparation VARCHAR (50) ,
+    genus_species VARCHAR(500),
+    etiology_status VARCHAR(250),
+    location_of_preparation VARCHAR(250),
     illnesses INT,
     hospitalizations INT,
     deaths INT,
-    food_vehicle VARCHAR (50)
-	);
+    food_vehicle VARCHAR(500)
+);
 
 
-CREATE TABLE weather(
-	id INT PRIMARY KEY,
-	yearmonth DATE	,
-    state VARCHAR(20),
-    average_temp FLOAT(10)
-	);
+CREATE TABLE temperature (
+    id INT(10) PRIMARY KEY NOT NULL,
+    yearmonth DATE,
+    state VARCHAR(50),
+    average_temp FLOAT
+);
     
     
