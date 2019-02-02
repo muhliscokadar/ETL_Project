4a22623105c8157749186656e721ce8349bee33b
bharathiyer <bharathiyer@gmail.com>
2019-02-02 12:22:34 -0600

    schema and query sql files

---
+++ etl/query.sql
@@ -0,0 +1,5 @@
+USE outbreak_db;
+
+-- Query to check successful load
+
+-- Join tables on county_id
---
+++ etl/schema.sql
@@ -0,0 +1,7 @@
+-- Drops the outbreak_db if it already exists --
+DROP DATABASE IF EXISTS outbreak_db;
+-- Create and use outbreak_db
+CREATE DATABASE outbreak_db;
+USE outbreak_db;
+
+-- Create Two Tables
