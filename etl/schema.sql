7e429cd41f3f186a1e89d6482026349ce3965538
Tejaswini Udipi <wini296@gmail.com>
2019-02-02 13:54:37 -0600

    schema update

--- etl/schema.sql
+++ etl/schema.sql
@@ -5,3 +5,25 @@ CREATE DATABASE outbreak_db;
 USE outbreak_db;
 
 -- Create Two Tables
+CREATE TABLE food_data(
+	id INT(10) PRIMARY KEY NOT NULL,
+    yearmonth DATE,
+    state VARCHAR(50),
+    genus_species VARCHAR (50), 
+    etiology_status VARCHAR (20),
+    location_of_preparation VARCHAR (50) ,
+    illnesses INT,
+    hospitalizations INT,
+    deaths INT,
+    food_vehicle VARCHAR (50)
+	);
+
+
+CREATE TABLE weather(
+	id INT PRIMARY KEY,
+	yearmonth DATE	,
+    state VARCHAR(20),
+    average_temp FLOAT(10)
+	);
+    
+    
